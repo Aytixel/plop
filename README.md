8c81cd5110440d92739b34a4bf940820edd5a277
Aytixel <aytixel@gmail.com>
2023-06-16 02:15:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Plop
+
+**Plop est une plateforme de vidéo légère et gratuite,** permettant à n'importe qui de mettre en ligne des vidéos avec son compte.
+Cette *plateforme a pour but d'être la plus légère possible côté serveur,*
+pour cela **l'encodage** des différentes résolutions vidéos **est réalisé dans le navigateur** qui met en ligne la vidéo.
+
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", name)
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
