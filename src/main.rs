cddff45d4863e7d7d04fa001033f9c921bf4e66f
Aytixel <aytixel@gmail.com>
2023-07-04 04:30:21 +0200

    Setuping the server

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,56 @@
-use std::io;
+use std::{
+    fs::File,
+    io::{self, BufReader, Error, ErrorKind},
+};
 
-use actix_web::{get, web, App, HttpServer, Responder};
-
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", name)
-}
+use actix_cors::Cors;
+use actix_files::Files;
+use actix_web::{middleware, App, HttpServer};
+use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{certs, pkcs8_private_keys};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| App::new().service(greet))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default().allow_any_origin();
+
+        App::new()
+            .wrap(cors)
+            .wrap(middleware::Compress::default())
+            .wrap(
+                middleware::DefaultHeaders::new()
+                    .add(("Cross-Origin-Embedder-Policy", "require-corp"))
+                    .add(("Cross-Origin-Opener-Policy", "same-origin")),
+            )
+            .service(Files::new("/", "./static/").index_file("index.html"))
+    })
+    .bind_rustls("127.0.0.1:8080", load_rustls_config()?)?
+    .run()
+    .await
+}
+
+fn load_rustls_config() -> io::Result<ServerConfig> {
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth();
+    let cert_chain = certs(&mut BufReader::new(File::open("cert/cert.pem")?))?
+        .into_iter()
+        .map(Certificate)
+        .collect();
+    let mut keys: Vec<PrivateKey> =
+        pkcs8_private_keys(&mut BufReader::new(File::open("cert/key.pem")?))?
+            .into_iter()
+            .map(PrivateKey)
+            .collect();
+
+    if keys.is_empty() {
+        return Err(Error::new(
+            ErrorKind::NotFound,
+            "Could not locate PKCS 8 private keys.",
+        ));
+    }
+
+    Ok(config
+        .with_single_cert(cert_chain, keys.remove(0))
+        .map_err(|error| Error::new(ErrorKind::InvalidData, error.to_string()))?)
 }
