b7aef3228f0c2a5e563820f1a9edfd4c2960fb00
Aytixel <aytixel@gmail.com>
2023-08-14 18:00:00 +0200

    Update watch.js

--- static/js/watch.js
+++ static/js/watch.js
@@ -6,18 +6,18 @@ const time_ago = (async () => {
 
 // video description
 {
-    const vues_element = document.getElementById("vues")
+    const vues = document.getElementById("vues")
 
     if (video_metadata.vues >= 0 && video_metadata.vues < 1000)
-        vues_element.textContent = video_metadata.vues + (video_metadata.vues > 1 ? " vues" : " vue")
+        vues.textContent = video_metadata.vues + (video_metadata.vues > 1 ? " vues" : " vue")
     else if (video_metadata.vues < 1000000)
-        vues_element.textContent = (Math.round(video_metadata.vues / 100) / 10) + " k"
+        vues.textContent = (Math.round(video_metadata.vues / 100) / 10) + " k"
     else if (video_metadata.vues < 1000000000)
-        vues_element.textContent = (Math.round(video_metadata.vues / 100000) / 10) + " M de vues"
+        vues.textContent = (Math.round(video_metadata.vues / 100000) / 10) + " M de vues"
     else
-        vues_element.textContent = (Math.round(video_metadata.vues / 100000000) / 10) + " Md de vues"
+        vues.textContent = (Math.round(video_metadata.vues / 100000000) / 10) + " Md de vues"
 
-    const time_element = document.getElementById("time")
+    const time = document.getElementById("time")
 
-    time_ago.then(time_ago => time_element.textContent = time_ago.format(video_metadata.date.valueOf()))
+    time_ago.then(time_ago => time.textContent = time_ago.format(video_metadata.date.valueOf()))
 }
