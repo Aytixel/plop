5471bf937a54513a038adb1ac7206b54d6cc8d89
Aytixel <aytixel@gmail.com>
2023-08-15 14:47:00 +0200

    Add audio encoding to the new video encoder

--- static/js/upload.js
+++ static/js/upload.js
@@ -102,6 +102,50 @@ function get_video_encode_options_list(width, height, framerate) {
     }).filter((encode_options, index) => index == 0 || encode_options.width <= width)
 }
 
+function encode_audio(url, audio_config, callback) {
+    return new Promise((resolve, reject) => {
+        const video = document.createElement("video")
+
+        video.src = url
+        video.addEventListener("loadedmetadata", async () => {
+            const audio_encoder = new AudioEncoder({
+                output: callback,
+                error: reject,
+            })
+
+            audio_config = Object.assign(audio_config, {
+                codec: "opus",
+                bitrate: 128000
+            })
+
+            console.log(audio_config)
+
+            audio_encoder.configure(audio_config)
+
+            const audio_processor = new MediaStreamTrackProcessor(video.audioTracks[0])
+            const audio_reader = audio_processor.readable.getReader()
+
+            video.play()
+
+            while (true) {
+                const result = await audio_reader.read()
+
+                if (result.done)
+                    break
+
+                const audio_data = result.value
+
+                audio_encoder.encode(audio_data)
+                audio_data.close()
+            }
+
+            audio_encoder.close()
+
+            resolve()
+        })
+    })
+}
+
 const encoding_bitrate_multiplier = 0.12
 
 function encode_video(url, encode_options_list, callback) {
@@ -112,6 +156,27 @@ function encode_video(url, encode_options_list, callback) {
 
         video.src = url
         video.addEventListener("loadedmetadata", async () => {
+            let audio_config
+
+            // setup audio encoder
+            if (video.audioTracks.length) {
+                const audio_processor = new MediaStreamTrackProcessor(video.audioTracks[0])
+                const audio_reader = audio_processor.readable.getReader()
+
+                video.play()
+
+                const audio_data = (await audio_reader.read()).value
+
+                video.pause()
+
+                video.currentTime = 0
+                audio_config = {
+                    numberOfChannels: audio_data.numberOfChannels,
+                    sampleRate: audio_data.sampleRate
+                }
+            }
+
+            // setup video encoder and the muxer
             for (const key in encode_options_list) {
                 const muxer_config = {
                     target: new WebMMuxer.StreamTarget(
@@ -131,29 +196,33 @@ function encode_video(url, encode_options_list, callback) {
                             chunkSize: 1_500_000
                         }
                     ),
-                    type: "webm",
                     video: {
-                        codec: 'V_VP9',
+                        codec: "V_VP9",
                         width: encode_options_list[key].width,
                         height: encode_options_list[key].height,
                         frameRate: encode_options_list[key].framerate,
                         alpha: false
                     },
                     streaming: true,
-                    firstTimestampBehavior: "permissive"
+                    type: "webm"
                 }
+
+                if (video.audioTracks.length)
+                    muxer_config.audio = Object.assign({ ...audio_config }, {
+                        codec: "A_OPUS"
+                    })
 
                 const muxer = new WebMMuxer.Muxer(muxer_config)
                 const video_encoder = new VideoEncoder({
-                    output: (chunk, meta) => muxer.addVideoChunk(chunk, meta),
-                    error: (error) => reject(error),
+                    output: (chunk, metadata) => muxer.addVideoChunk(chunk, metadata),
+                    error: reject,
                 })
                 const video_encoder_config = {
                     codec: "vp09.02.10.10.01",
-                    height: encode_options_list[key].height,
                     width: encode_options_list[key].width,
-                    framerate: encode_options_list[key].framerate,
-                    bitrate: encode_options_list[key].width * encode_options_list[key].height * encode_options_list[key].framerate * encoding_bitrate_multiplier
+                    height: encode_options_list[key].height,
+                    bitrate: encode_options_list[key].width * encode_options_list[key].height * encode_options_list[key].framerate * encoding_bitrate_multiplier,
+                    framerate: encode_options_list[key].framerate
                 }
                 let video_encoder_configured = false
 
@@ -180,59 +249,76 @@ function encode_video(url, encode_options_list, callback) {
                 encode_options_list[key].timestamp = 0
             }
 
+            let audio_encoder
+
+            // start audio encoding
+            if (video.audioTracks.length) {
+                let start_timestamp = -1
+
+                audio_encoder = encode_audio(url, audio_config, (chunk, metadata) => {
+                    if (start_timestamp == -1)
+                        start_timestamp = chunk.timestamp
+
+                    for (encode_options of encode_options_list) {
+                        encode_options.muxer.addAudioChunk(chunk, metadata, chunk.timestamp - start_timestamp)
+                    }
+                })
+            }
+
+            // start video encoding
             let frame_count = 0
             const duration = Math.round(video.duration * 1_000_000)
             const high_encode_options = encode_options_list[encode_options_list.length - 1]
 
-            function encode_frame() {
-                async () => {
-                    console.log(`Time : ${video.currentTime}s`)
+            async function encode_frame() {
+                console.log(`Time : ${video.currentTime}s`)
 
-                    frame_count++
+                frame_count++
 
-                    const frame = new VideoFrame(video, {
-                        timestamp: high_encode_options.timestamp,
-                        duration
-                    })
+                const video_frame = new VideoFrame(video, {
+                    timestamp: high_encode_options.timestamp,
+                    duration
+                })
 
-                    for (const encode_options of encode_options_list) {
-                        if (
-                            high_encode_options.framerate == encode_options.framerate
-                            || (video.currentTime * 1_000_000) >= encode_options.timestamp
-                        ) {
-                            encode_options.timestamp = Math.min(encode_options.timestamp + Math.round(1_000_000 / encode_options.framerate), duration)
+                for (const encode_options of encode_options_list) {
+                    if (
+                        high_encode_options.framerate == encode_options.framerate
+                        || (video.currentTime * 1_000_000) >= encode_options.timestamp
+                    ) {
+                        encode_options.timestamp = Math.min(encode_options.timestamp + Math.round(1_000_000 / encode_options.framerate), duration)
 
-                            encode_options.video_encoder.encode(frame, {
-                                keyFrame: frame_count % (encode_options.framerate * 4) == 0
-                            })
-                        }
+                        encode_options.video_encoder.encode(video_frame, {
+                            keyFrame: frame_count % (encode_options.framerate * 4) == 0
+                        })
                     }
-
-                    frame.close()
-
-                    if (high_encode_options.video_encoder.encodeQueueSize > 4)
-                        await high_encode_options.video_encoder.flush()
-
-                    if (video.currentTime != video.duration) {
-                        video.currentTime = high_encode_options.timestamp / 1_000_000
-
-                        return
-                    }
-
-                    // ending frame
-                    for (const encode_options of encode_options_list) {
-                        if (encode_options.video_encoder.state != "closed")
-                            encode_options.video_encoder.close()
-
-                        encode_options.muxer.finalize()
-                    }
-
-                    console.timeEnd("Encode time")
-                    console.log(`Frame count : ${frame_count}`)
-                    video.removeEventListener("timeupdate", encode_frame)
-
-                    resolve()
                 }
+
+                video_frame.close()
+
+                if (high_encode_options.video_encoder.encodeQueueSize > 4)
+                    await high_encode_options.video_encoder.flush()
+
+                if (video.currentTime != video.duration) {
+                    video.currentTime = high_encode_options.timestamp / 1_000_000
+
+                    return
+                }
+
+                // ending frame
+                for (const encode_options of encode_options_list) {
+                    if (encode_options.video_encoder.state != "closed")
+                        encode_options.video_encoder.close()
+
+                    encode_options.muxer.finalize()
+                }
+
+                await audio_encoder
+
+                console.timeEnd("Encode time")
+                console.log(`Frame count : ${frame_count}`)
+                video.removeEventListener("timeupdate", encode_frame)
+
+                resolve()
             }
 
             video.addEventListener("timeupdate", encode_frame)
